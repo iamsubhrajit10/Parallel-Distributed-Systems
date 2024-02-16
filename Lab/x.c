621275af3f54c75da9e2a94ecb13f54e1e94ce3b
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-16 19:31:19 +0530

    Update x.c

--- Lab/x.c
+++ Lab/x.c
@@ -76,13 +76,12 @@ void receive_permutations(int process_id, int num_processes, int max_length, int
     char **received_permutations = (char **)malloc((strings_per_process + 1) * sizeof(char *));
     for (int i = 0; i <= strings_per_process; i++) {
         received_permutations[i] = (char *)malloc((max_length + 1) * sizeof(char));
-       
     }
 
     // Receive permutations from all processes except the last one
-    for (int src = 0; src < num_processes - 1; src++) {
+    for (int src = 0; src < num_processes; src++) {
         MPI_Recv(received_permutations[0], (max_length + 1) * (strings_per_process + 1), MPI_CHAR, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-         printf("%s\n",received_permutations[0]);
+        printf("%s\n",received_permutations[0]);
         // Copy received permutations to all_permutations array
         for (int i = 0; i <= strings_per_process; i++) {
             strcpy(all_permutations[local_count], received_permutations[i]);
@@ -91,15 +90,15 @@ void receive_permutations(int process_id, int num_processes, int max_length, int
                 break;
             }
         }
-
     }
 
     // Free memory allocated for received_permutations
-    // for (int i = 0; i <= strings_per_process; i++) {
-    //     free(received_permutations[i]);
-    // }
-    // free(received_permutations);
+    for (int i = 0; i <= strings_per_process; i++) {
+        free(received_permutations[i]);
+    }
+    free(received_permutations);
 }
+
 
 
 int main(int argc, char *argv[]) {
