bdd3aa21193863dc7a2bc6d55ae0dba71966bdf5
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-16 19:46:33 +0530

    Update x.c

--- Lab/x.c
+++ Lab/x.c
@@ -79,7 +79,7 @@ void generate_strings(int process_id, int num_processes, int max_length, int tot
     // }
     // Send the whole permutations array to the last process
     char *flattened_permutations = flatten(permutations, strings_per_process+1, max_length+1);
-    MPI_Send(&flatenned_permutations, (max_length + 1) * (strings_per_process + 1), MPI_CHAR, num_processes - 1, 0, MPI_COMM_WORLD);
+    MPI_Send(&flattened_permutations, (max_length + 1) * (strings_per_process + 1), MPI_CHAR, num_processes - 1, 0, MPI_COMM_WORLD);
 
     // Free memory allocated for permutations
     for (int i = 0; i <= strings_per_process; i++) {
