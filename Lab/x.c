19a57831f002490ab4eeaa0946f1d938a83beb86
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-16 19:35:44 +0530

    Update x.c

--- Lab/x.c
+++ Lab/x.c
@@ -51,10 +51,10 @@ void generate_strings(int process_id, int num_processes, int max_length, int tot
 
     // Generate permutations and store them in the 2D array
     generate_permutations(char_set, start_index, end_index , max_length, "", 0, permutations, &counter, strings_per_process + 1);
-    for (int i = 0; i <= strings_per_process; i++) {
-        // x_permutations[i-1]=permutations[i];
-        printf("\n%s\n", permutations[i]);
-    }
+    // for (int i = 0; i <= strings_per_process; i++) {
+    //     // x_permutations[i-1]=permutations[i];
+    //     printf("\n%s\n", permutations[i]);
+    // }
     // Send the whole permutations array to the last process
     MPI_Send(permutations[0], (max_length + 1) * (strings_per_process + 1), MPI_CHAR, num_processes - 1, 0, MPI_COMM_WORLD);
 
