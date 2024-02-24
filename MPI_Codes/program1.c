531c06eb1520cb9a7eefbe0710147c9ebb0a32c8
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-25 00:28:51 +0530

    Update program1.c

--- MPI_Codes/program1.c
+++ MPI_Codes/program1.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (size != 2) {
+    if (size != 5) {
         printf("This program must be run with 2 processes.\n");
         MPI_Finalize();
         return 1;
