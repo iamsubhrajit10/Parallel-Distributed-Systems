68063b8170e7b90d4601f6b1b041285172752e8a
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-25 11:39:47 +0530

    Update DNA_iterative.c

--- Lab/DNA_iterative.c
+++ Lab/DNA_iterative.c
@@ -26,7 +26,7 @@ int main() {
 
     // Generate DNA strings
     int count = 4;
-    for (int length = 1; length <= 10; length++) {
+    for (int length = 1; length <= 7; length++) {
         int index = 0;
         for (int j = 0; j < 4; j++) {
             for (int i = 0; i < count; i++) {
