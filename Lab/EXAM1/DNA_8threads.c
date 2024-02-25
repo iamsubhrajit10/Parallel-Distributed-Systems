27332a5d985f98d33d1ca99cb3be9ef00f0c220c
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-25 11:58:13 +0530

    Update DNA_8threads.c

--- Lab/EXAM1/DNA_8threads.c
+++ Lab/EXAM1/DNA_8threads.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 
 #define ROWS 11
 #define COLS 65536
@@ -10,7 +11,7 @@
 int main() {
     // Allocate memory for the DNA_String array
     char DNA[] = {'A', 'C', 'G', 'T'};
-    int count_array[10]; // Array to store the count of DNA strings for each length
+    int count_array[LENGTH]; // Array to store the count of DNA strings for each length
     char (*DNA_String)[COLS][Z] = malloc(ROWS * sizeof(*DNA_String));
     
     if (DNA_String == NULL) {
@@ -22,7 +23,7 @@ int main() {
     for (int i = 0; i < 4; i++) {
         DNA_String[0][i][0] = DNA[i];
         DNA_String[0][i][1] = '\0'; // Null terminator
-        printf("%s\n",DNA_String[0][i]);
+        printf("%s\n", DNA_String[0][i]);
     }
 
     // Generate DNA strings
@@ -30,20 +31,19 @@ int main() {
     for (int length = 1; length <= LENGTH; length++) {
         int index = 0;
         for (int j = 0; j < 4; j++) {
-            int i=0;
-          #pragma omp parallel for num_threads(8) shared(index,j,length,DNA_String)
-            for (i = 0; i < count; i++) {
+            #pragma omp parallel for num_threads(8) private(index) shared(length, DNA_String, count)
+            for (int i = 0; i < count; i++) {
                 strcpy(DNA_String[length][index], DNA_String[length - 1][i]);
-                DNA_String[length][index][strlen(DNA_String[length][index])] = DNA[j];
-                DNA_String[length][index][strlen(DNA_String[length][index])+1] = '\0';
+                int curr_len = strlen(DNA_String[length][index]);
+                DNA_String[length][index][curr_len] = DNA[j];
+                DNA_String[length][index][curr_len + 1] = '\0';
                 index++;
             }
-          #pragma omp barrier
         }
         count = index; // Update count for the next length
         count_array[length - 1] = count; // Store the count for this length
     }
-   
+
     // Print the DNA strings
     for (int len = 1; len <= LENGTH; len++) {
         printf("DNA strings of length %d:\n", len);
