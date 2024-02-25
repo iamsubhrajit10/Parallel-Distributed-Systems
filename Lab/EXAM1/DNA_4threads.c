aa3e1b707ed232e39b23f41772f3a315de40d267
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2024-02-25 11:53:28 +0530

    Update DNA_4threads.c

--- Lab/EXAM1/DNA_4threads.c
+++ Lab/EXAM1/DNA_4threads.c
@@ -30,8 +30,9 @@ int main() {
     for (int length = 1; length <= LENGTH; length++) {
         int index = 0;
         for (int j = 0; j < 4; j++) {
-          #pragma omp parallel for num_threads(4)
-            for (int i = 0; i < count; i++) {
+            int i=0;
+          #pragma omp parallel for num_threads(4) shared(index)
+            for (i = 0; i < count; i++) {
                 strcpy(DNA_String[length][index], DNA_String[length - 1][i]);
                 DNA_String[length][index][strlen(DNA_String[length][index])] = DNA[j];
                 DNA_String[length][index][strlen(DNA_String[length][index])+1] = '\0';
